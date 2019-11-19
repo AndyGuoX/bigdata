90eb7ce172f4d06a6a9d5e3027fc6ee11e2c065b
andyguo <1353418770@qq.com>
2019-11-19 09:41:30 +0800

    新的项目

--- src/components/charts/Funnel.vue
+++ src/components/charts/Funnel.vue
@@ -28,67 +28,67 @@
       }
     },
     mounted() {
-    	let _this = this;
-        let data = _this.data;
-		let option = {
-			title: {
-		        text: data.name,
-		        textStyle:{
-		        	color:'rgba(255,255,255,.7)'
-		        },
-		        left:'center'
-		    },
-		    tooltip: {
-		        trigger: 'item',
-		        formatter: "{a} <br/>{b} : {c}%",
-		        textStyle:{
-		        	align:'left'
-		        }
-		    },
-		    legend: {
-		        data: data.legend,
-		        textStyle:{
-		        	color:'rgba(255,255,255,.7)',
-		        	fontSize:12
-		        },
-		        itemWidth:12,
-		        itemHeight:10,
-		        left:'center',
-		        bottom:0,
-		    },
-		    calculable: true,
-		    series: [
-		        {
-		            name:data.name,
-		            type:'funnel',
-		            left: 'center',
-		            width:'80%',
-		            top: 35,
-		            bottom: 45,
-		            min: 0,
-		            max: 100,
-		            minSize: '0%',
-		            maxSize: '100%',
-		            sort: data.sortType,
-		            gap: 0,
-		            label:{
-		            	normal:{
-		            		show:false,
-		            		position:'inside'
-		            	},
-		            	emphasis:{
-		            		show:true,
-		            		color:'#fff'
-		            	}
-		            },
-		            itemStyle: {
-		                borderWidth:0
-		            },
-		            data:data.data
-		        }
-		    ]
-		}
-   
+      let _this = this
+      let data = _this.data
+      let option = {
+        title: {
+          text: data.name,
+          textStyle: {
+            color: 'rgba(255,255,255,.7)'
+          },
+          left: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: "{a} <br/>{b} : {c}%",
+          textStyle: {
+            align: 'left'
+          }
+        },
+        legend: {
+          data: data.legend,
+          textStyle: {
+            color: 'rgba(255,255,255,.7)',
+            fontSize: 12
+          },
+          itemWidth: 12,
+          itemHeight: 10,
+          left: 'center',
+          bottom: 0,
+        },
+        calculable: true,
+        series: [
+          {
+            name: data.name,
+            type: 'funnel',
+            left: 'center',
+            width: '80%',
+            top: 35,
+            bottom: 45,
+            min: 0,
+            max: 100,
+            minSize: '0%',
+            maxSize: '100%',
+            sort: data.sortType,
+            gap: 0,
+            label: {
+              normal: {
+                show: false,
+                position: 'inside'
+              },
+              emphasis: {
+                show: true,
+                color: '#fff'
+              }
+            },
+            itemStyle: {
+              borderWidth: 0
+            },
+            data: data.data
+          }
+        ]
+      }
+
       this.myChart = echarts.init(this.$refs.funnel)
       this.myChart.setOption(option)
     }
